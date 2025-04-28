efa18e5a503d830ec5e41f5e3e55a49561c870f0
Avaz <avazbekrozimboyev08@gmail.com>
2025-04-28 17:40:57 +0500

    fgdhjk

--- src/hooks/useAxios/index.js
+++ src/hooks/useAxios/index.js
@@ -3,13 +3,15 @@ import axios from "axios";
 export const useAxios = () => {
   const request = ({ url, method, body, params }) => {
     return axios({
-      url: `${import.meta.VITE_BASE_URL}/${url}`,
+      url: `${import.meta.env.VITE_BASE_URL}/${url}`,
       method,
       data: body,
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${localStorage.getItem("token")}`,
       },
-    });
+      params,
+    }).then(res=>res.data.data)
   };
+  return request;
 };
--- src/pages/sign-in/signIn.jsx
+++ src/pages/sign-in/signIn.jsx
@@ -1,7 +1,56 @@
-import React from "react";
+import { Button, Form, Input } from "antd";
+import Password from "antd/es/input/Password";
+import React, { useState } from "react";
+import { useAxios } from "../../hooks/useAxios";
+import { Loader } from "lucide-react";
 
 const SignIn = () => {
-  return <div>SignIN </div>;
+  const axios = useAxios();
+  const [loading, setLoading] = useState(false);
+  const login = (e) => {
+    setLoading(true);
+    axios({ url: "api/auth/sign-in", method: "POST", body: e })
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  };
+  return (
+    <div className="h-screen w-[400px] flex items-center justify-center m-auto flex-col ">
+      <h1 className="text-2xl  font-bold">Login</h1>
+      <div className="mt-5 w-full border border-[#e2dfdf] p-4 bg-white rounded-lg shadow-lg flex flex-col gap-4">
+        <Form onFinish={login} className="">
+          <Form.Item
+            name={"email"}
+            rules={[
+              {
+                required: true,
+                message: "Please fill in the field !",
+              },
+            ]}
+          >
+            <Input placeholder="Email...." autoComplete="email" />
+          </Form.Item>
+          <Form.Item
+            name={"password"}
+            rules={[
+              {
+                required: true,
+                message: "Please fill in the field !",
+              },
+            ]}
+          >
+            <Input.Password
+              placeholder="******"
+              autoComplete="current-password"
+            />
+          </Form.Item>
+          <Button htmlType="submit" className="w-full" type="primary">
+            {loading ? <Loader className="animate-spin" /> : " Login"}
+          </Button>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
